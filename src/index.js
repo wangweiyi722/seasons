159e577929a58e3650479d1d128238b3d010d490
Weiyi Wang <weiyiwang@google.com>
2019-04-04 09:06:13 -0500

    replaced default components with custom components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  constructor(props){
+    super(props);
+
+    //only time we do direct assignment to state
+    this.state = {lat: null, errorMessage: ''};
+
+  };
+
+  componentDidMount(){
+    window.navigator.geolocation.getCurrentPosition(
+      // Callback for position;
+      (position) => this.setState({lat: position.coords.latitude}),
+      (err) => this.setState({errorMessage: err.message})
+    );
+  }
+
+  render() {
+
+    if(this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if(!this.state.errorMessage && this.state.lat) {
+      return <div><SeasonDisplay latitude={this.state.lat}/></div>
+    }
+
+    return <div> Loading </div>
+    /*
+    return (
+      <div><SeasonDisplay/>
+      Latitude: {this.state.lat} <br/>
+      Error: {this.state.errorMessage}</div>
+    );
+    */
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
